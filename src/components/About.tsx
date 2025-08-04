d09b56cb1ec0e4561444a4a6e3537c957fd5d61b
Alex Leung <alex@alexleung.ca>
2025-08-04 18:25:39 -0400

    Update About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,14 +9,28 @@ const About = () => {
       <div className="md:grid md:grid-cols-[3fr_2fr] md:gap-x-16 md:pt-8">
         <div className="text-left leading-relaxed text-lg mb-8 md:w-[90%]">
           <p className="mb-6">
-            I love applying effective software design to craft beautiful, compelling, 
-            and intuitive solutions to engineering problems. In my 
-            spare time I enjoy reading 📚, rock climbing 🧗, and spending time with furmily 🐱.
+            I love applying effective software design to craft beautiful,
+            compelling, and intuitive solutions to engineering problems. In my
+            spare time I enjoy playing tennis 🎾, reading 📚, hiking 🏔️, rock
+            climbing 🧗, and spending time with furmily 🐱.
           </p>
 
           <Skills />
         </div>
-        <Image src="/assets/about.jpg" alt="Alex and Galactica" width={400} height={400} />
+        <div className="flex flex-col gap-4 md:gap-6">
+          <Image
+            src="/assets/about_portrait.webp"
+            alt="Alex on a hike"
+            width={400}
+            height={400}
+          />
+          <Image
+            src="/assets/about_alex_and_galactica.webp"
+            alt="Alex and Galactica"
+            width={400}
+            height={400}
+          />
+        </div>
       </div>
     </section>
   );
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,10 +3,13 @@ import { skills } from "@/constants/skills";
 const Skills = () => {
   return (
     <>
-      Here are a few things that I like:
+      Here are a few technical areas that I enjoy working in:
       <ul className="grid grid-cols-[repeat(2,minmax(130px,200px))] gap-x-4 list-none mt-4 p-0">
         {skills.map(({ skill }) => (
-          <li key={skill} className="relative pl-6 mb-3 leading-6 before:content-['■'] before:absolute before:left-0 before:text-base before:top-0">
+          <li
+            key={skill}
+            className="relative pl-6 mb-3 leading-6 before:content-['■'] before:absolute before:left-0 before:text-base before:top-0"
+          >
             {skill}
           </li>
         ))}
