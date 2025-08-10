5d5d42ca73d1c543c252ab1c224dfdffce83d82b
Alex Leung <alex@alexleung.ca>
2025-08-10 19:57:30 -0400

    Fix background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,15 @@ import Contact from "@/components/Contact";
 
 export default function Page() {
   return (
-    <div className="bg-black text-white min-h-screen bg-[url('/assets/background.webp')] bg-cover bg-center bg-fixed bg-no-repeat bg-overlay">
-      <SocialLinks />
-      <Home />
-      <About />
-      <Contact />
-      <Footer />
-    </div>
+    <>
+      <div className="fixed inset-0 h-screen bg-[url('/assets/background.webp')] bg-cover bg-center bg-no-repeat -z-10 after:absolute after:inset-0 after:bg-black/50"></div>
+      <div className="text-white min-h-screen">
+        <SocialLinks />
+        <Home />
+        <About />
+        <Contact />
+        <Footer />
+      </div>
+    </>
   );
 }
