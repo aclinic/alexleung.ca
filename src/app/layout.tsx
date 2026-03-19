4275c6dc472066fff9bd109cd8c821cb38a24b10
Alex Leung <alex@alexleung.ca>
2026-03-18 21:05:05 -0700

    revert: keep analytics script outside body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -103,10 +103,8 @@ export default function RootLayout({ children }: PropsWithChildren) {
         <JsonLd item={buildPersonSchema({ description })} />
         <JsonLd item={buildWebsiteSchema({ description })} />
         <JsonLd item={buildProfessionalServiceSchema({ description })} />
-        {googleAnalyticsId ? (
-          <GoogleAnalytics gaId={googleAnalyticsId} />
-        ) : null}
       </body>
+      {googleAnalyticsId ? <GoogleAnalytics gaId={googleAnalyticsId} /> : null}
     </html>
   );
 }
